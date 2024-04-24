56772a35deee389bfc2d654d3405bf3f6ac0ea20
Stanislav Basarab <stanislavbasarab083@gmail.com>
2024-04-24 13:29:04 +0300

    Prepare window for drawing

--- ui/window.go
+++ ui/window.go
@@ -16,6 +16,10 @@ import (
 	"golang.org/x/mobile/event/size"
 )
 
+const (
+	WindowSide = 800
+)
+
 type Visualizer struct {
 	Title         string
 	Debug         bool
@@ -25,15 +29,16 @@ type Visualizer struct {
 	tx   chan screen.Texture
 	done chan struct{}
 
-	sz  size.Event
-	pos image.Rectangle
+	sz     size.Event
+	center image.Point
 }
 
 func (pw *Visualizer) Main() {
 	pw.tx = make(chan screen.Texture)
 	pw.done = make(chan struct{})
-	pw.pos.Max.X = 200
-	pw.pos.Max.Y = 200
+	center := WindowSide / 2
+	pw.center.X = center
+	pw.center.Y = center
 	driver.Main(pw.run)
 }
 
@@ -43,7 +48,9 @@ func (pw *Visualizer) Update(t screen.Texture) {
 
 func (pw *Visualizer) run(s screen.Screen) {
 	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title: pw.Title,
+		Title:  pw.Title,
+		Width:  WindowSide,
+		Height: WindowSide,
 	})
 	if err != nil {
 		log.Fatal("Failed to initialize the app window:", err)
@@ -131,9 +138,7 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 }
 
 func (pw *Visualizer) drawDefaultUI() {
-	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
-
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+	pw.w.Fill(pw.sz.Bounds(), color.White, draw.Src) // Фон.
 
 	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
