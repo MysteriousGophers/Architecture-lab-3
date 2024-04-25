ebb687b57a1619b4fd5fd9ff8284be5ccedbe266
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-04-25 21:07:52 +0200

    Reformat `parser` logic

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -3,27 +3,21 @@ package lang
 import (
 	"bufio"
 	"fmt"
-	"github.com/mysteriousgophers/architecture-lab-3/painter"
-	"io"
 	"strconv"
 	"strings"
+
+	"github.com/mysteriousgophers/architecture-lab-3/painter"
+	"io"
 )
 
-type Parser struct {
-	lastBgColor painter.Operation
-	lastBgRect  *painter.BgRect
-	figures     []*painter.TFigure
-	moveOps     []painter.Operation
-	updateOp    painter.Operation
-}
+const screenSize = 800
 
-func (p *Parser) initialize() {
-	if p.lastBgColor == nil {
-		p.lastBgColor = painter.OperationFunc(painter.Reset)
-	}
-	if p.updateOp != nil {
-		p.updateOp = nil
-	}
+type Parser struct {
+	bgColor  painter.Operation
+	bgRect   *painter.BgRect
+	figures  []*painter.TFigure
+	moveOps  []painter.Operation
+	updateOp painter.Operation
 }
 
 func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
@@ -33,83 +27,98 @@ func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
 
 	for scanner.Scan() {
 		commandLine := scanner.Text()
-
-		err := p.parse(commandLine)
-		if err != nil {
+		if err := p.parse(commandLine); err != nil {
 			return nil, err
 		}
 	}
 	return p.finalResult(), nil
 }
 
-func (p *Parser) finalResult() []painter.Operation {
-	var res []painter.Operation
-	if p.lastBgColor != nil {
-		res = append(res, p.lastBgColor)
-	}
-	if p.lastBgRect != nil {
-		res = append(res, p.lastBgRect)
-	}
-	if len(p.moveOps) != 0 {
-		res = append(res, p.moveOps...)
-	}
-	p.moveOps = nil
-	if len(p.figures) != 0 {
-		println(len(p.figures))
-		for _, figure := range p.figures {
-			res = append(res, figure)
-		}
-	}
-	if p.updateOp != nil {
-		res = append(res, p.updateOp)
-	}
-	return res
-}
-
-func (p *Parser) resetState() {
-	p.lastBgColor = nil
-	p.lastBgRect = nil
-	p.figures = nil
-	p.moveOps = nil
-	p.updateOp = nil
-}
-
 func (p *Parser) parse(commandLine string) error {
-	parts := strings.Split(commandLine, " ")
-	instruction := parts[0]
-	var args []string
-	if len(parts) > 1 {
-		args = parts[1:]
+	tokens := strings.Split(commandLine, " ")
+	if len(tokens) < 1 {
+		return fmt.Errorf("invalid command format: %s", commandLine)
 	}
-	var iArgs []int
-	for _, arg := range args {
-		f, err := strconv.ParseFloat(arg, 64)
-		if err == nil {
-			i := int(f * 800)
-			iArgs = append(iArgs, i)
-		}
+
+	instruction := tokens[0]
+	args, err := toIntArgs(tokens, screenSize)
+	if err != nil {
+		return fmt.Errorf("invalid argument format for %s: %s", instruction, commandLine)
 	}
 
 	switch instruction {
 	case "white":
-		p.lastBgColor = painter.OperationFunc(painter.WhiteFill)
+		p.bgColor = painter.OperationFunc(painter.WhiteFill)
 	case "green":
-		p.lastBgColor = painter.OperationFunc(painter.GreenFill)
+		p.bgColor = painter.OperationFunc(painter.GreenFill)
 	case "bgrect":
-		p.lastBgRect = &painter.BgRect{X1: iArgs[0], Y1: iArgs[1], X2: iArgs[2], Y2: iArgs[3]}
+		if len(args) != 4 {
+			return fmt.Errorf("invalid number of arguments for bgrect: %s", commandLine)
+		}
+		p.bgRect = &painter.BgRect{X1: args[0], Y1: args[1], X2: args[2], Y2: args[3]}
 	case "figure":
-		figure := painter.TFigure{X: iArgs[0], Y: iArgs[1]}
-		p.figures = append(p.figures, &figure)
+		if len(args) != 2 {
+			return fmt.Errorf("invalid number of arguments for figure: %s", commandLine)
+		}
+		p.figures = append(p.figures, &painter.TFigure{X: args[0], Y: args[1]})
 	case "move":
-		moveOp := painter.MoveFigures{X: iArgs[0], Y: iArgs[1], Figures: p.figures}
-		p.moveOps = append(p.moveOps, &moveOp)
+		if len(args) != 2 {
+			return fmt.Errorf("invalid number of arguments for move: %s", commandLine)
+		}
+		p.moveOps = append(p.moveOps, &painter.MoveFigures{X: args[0], Y: args[1], Figures: p.figures})
 	case "reset":
 		p.resetState()
-		p.lastBgColor = painter.OperationFunc(painter.Reset)
+		p.bgColor = painter.OperationFunc(painter.Reset)
 	case "update":
 		p.updateOp = painter.UpdateOp
 	default:
-		return fmt.Errorf("could not parse command %v", commandLine)
+		return fmt.Errorf("unknown command: %s", instruction)
 	}
 	return nil
+}
+
+func toIntArgs(tokens []string, screenSize int) ([]int, error) {
+	args := make([]int, 0, len(tokens)-1)
+	for _, arg := range tokens[1:] {
+		val, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			return nil, err
+		}
+		args = append(args, int(val*float64(screenSize)))
+	}
+	return args, nil
+}
+
+func (p *Parser) finalResult() []painter.Operation {
+	var result []painter.Operation
+	result = append(result, p.bgColor)
+	if p.bgRect != nil {
+		result = append(result, p.bgRect)
+	}
+	result = append(result, p.moveOps...)
+	p.moveOps = nil // Clear accumulated move operations
+	if len(p.figures) > 0 {
+		for _, figure := range p.figures {
+			result = append(result, figure)
+		}
+	}
+	if p.updateOp != nil {
+		result = append(result, p.updateOp)
+	}
+	return result
+}
+
+func (p *Parser) initialize() {
+	if p.bgColor == nil {
+		p.bgColor = painter.OperationFunc(painter.Reset)
+	}
+	p.updateOp = nil
+}
+
+func (p *Parser) resetState() {
+	p.bgRect = nil
+	p.bgRect = nil
+	p.figures = nil
+	p.moveOps = nil
+	p.updateOp = nil
 }
